1f98c73d6506f8c6f3f02ee8744f401d476c098a
parmalatinter <parmalatinter@gmail.com>
2019-12-09 18:32:18 +0900

    update for comple

---
+++ config/settings.inc.php
@@ -0,0 +1,17 @@
+<?php
+define('_DB_SERVER_', 'j1r4n2ztuwm0bhh5.cbetxkdyhwsb.us-east-1.rds.amazonaws.com');
+define('_DB_NAME_', 'zhldxqwbx9ziygj4');
+define('_DB_USER_', 't8hnnox7wn0auz1b');
+define('_DB_PASSWD_', 'gkblkmdfn28b1ema');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', '1CzQT4mdkDBjG5qtJ836PmvmRXGrbk2rL0TQSfVRnXwv5YXLI64M7CvS');
+define('_COOKIE_IV_', 'H77ACZqe');
+define('_PS_CREATION_DATE_', '2019-12-09');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.23');
+define('_QLOAPPS_VERSION_', '1.4.0');
+define('_RIJNDAEL_KEY_', '8Cu88VZ86ts14jTeQMiLwWkm3jbrESw1');
+define('_RIJNDAEL_IV_', 'nbjBDzoz8BRnlfXUDmd4mA==');
