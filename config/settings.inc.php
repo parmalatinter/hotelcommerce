61e599f3fd7ec2748fbdf49d27d0fb2729f9e52f
parmalatinter <parmalatinter@gmail.com>
2019-12-18 16:33:25 +0900

    add settings for run local

---
+++ config/settings.inc.php
@@ -0,0 +1,17 @@
+<?php
+define('_DB_SERVER_', 'localhost:3300');
+define('_DB_NAME_', 'local');
+define('_DB_USER_', 'root');
+define('_DB_PASSWD_', 'root');
+define('_DB_PREFIX_', 'ps_');
+define('_MYSQL_ENGINE_', 'InnoDB');
+define('_PS_CACHING_SYSTEM_', 'CacheMemcache');
+define('_PS_CACHE_ENABLED_', '0');
+define('_COOKIE_KEY_', 'SqL3KB1NPjGQyc2pdnFjzgFZgHUraEFwNoBLZbHQP4Yn8Wf3pXYUH3Rd');
+define('_COOKIE_IV_', 'HaSM1jUa');
+define('_PS_CREATION_DATE_', '2019-12-18');
+if (!defined('_PS_VERSION_'))
+	define('_PS_VERSION_', '1.6.1.23');
+define('_QLOAPPS_VERSION_', '1.4.0');
+define('_RIJNDAEL_KEY_', 'GRV4YMTAiv8ci4kMTNtygq1ewMcwr6pJ');
+define('_RIJNDAEL_IV_', 'O8YzlWXaBn0DAEC62h9p9A==');
